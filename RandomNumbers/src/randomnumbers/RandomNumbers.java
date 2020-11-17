bfec4153086fdb5b3b67970fcf0cad953d4224eb
Lee Osborne <71319749+leeosborne@users.noreply.github.com>
2020-11-17 11:02:22 +0000

    Add files via upload

---
+++ RandomNumbers/src/randomnumbers/RandomNumbers.java
@@ -0,0 +1,15 @@
+package randomnumbers;
+import java.util.*;
+
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+        int counter;
+        Random rnum = new Random();
+        for (counter = 1; counter <= 1; counter++) {
+            System.out.println(rnum.nextInt(100));
+        }
+    }
+    
+}
